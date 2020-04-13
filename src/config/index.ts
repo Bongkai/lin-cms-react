eecd9ebd17e6f3d1eac101d0fe41d77635d8da3c
Bongkai <849789181@qq.com>
2020-04-14 00:54:37 +0800

    Upgrade to version 0.1.0-beta.1

--- src/config/index.ts
+++ src/config/index.ts
@@ -10,6 +10,7 @@ interface AppConfigType {
 
 const Config: AppConfigType = {
   baseURL: 'http://localhost:5000',
+  // baseURL: 'http://pedro.7yue.pro',
   stagnateTime: 1 * 60 * 60 * 1000, // 无操作停滞时间  默认1小时
   openAutoJumpOut: false, // 是否开启无操作跳出
   notLoginRoute: ['login'], // 无需登录即可访问的路由 name,
